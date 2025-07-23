d0eb2dfd6b9cfe635d185052059f068444e04a1e
TatyanaDev <kot.kate2020@gmail.com>
2025-07-23 17:49:05 +0200

    add styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
+import { ReactNode } from "react";
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
       <body>{children}</body>
